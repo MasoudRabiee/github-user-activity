01f147512254db3b703568620aed0b838c4be214
MasoudRabiee <masoud.rabiee20@gmail.com>
2024-08-31 21:32:50 +0330

    Feat(bin): Add index.js functionality

--- bin/index.ts
+++ bin/index.ts
@@ -1,1 +1,20 @@
 #!/usr/bin/env node
+
+import { Command } from "commander";
+import { GithubCommand } from "../src/commands/github.command";
+
+const bootstrap = async () => {
+	const program = new Command();
+
+	program
+		.name("github-user-activity")
+		.description(
+			"A Cli program that fetches a user's recent GitHub activity and displays it in the terminal"
+		)
+		.version("1.0.0");
+
+	await new GithubCommand().load(program);
+	await program.parseAsync(process.argv);
+};
+
+bootstrap();
